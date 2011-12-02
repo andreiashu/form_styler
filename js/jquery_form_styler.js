7b84edface1e13f09a01f2fd49381b6c90c6fd94
Andrei Simion <a.simion@comicrelief.com>
2011-12-02 17:14:38 +0000

    Renamed module to form_styler

--- js/jquery_form_styler.js
+++
@@ -1,6 +0,0 @@
-
-Drupal.behaviors.jquery_form_styler = function (context) {
-  $('form.form-styler', context).each(function () {
-    $(this).jqTransform({imgPath: '/sites/all/modules/custom/jquery_form_styler/js/jqTransform/jqtransformplugin/img/'});
-  });
-};
