8e2d7f134cf24b99cd92ae3043ede53fc5474ae0
Andrei Simion <a.simion@comicrelief.com>
2011-12-05 11:58:34 +0000

    form_styler behavior uses Drupal.settings to pass options to jqTransform plugin

--- js/form_styler.js
+++ js/form_styler.js
@@ -1,6 +1,6 @@
 
 Drupal.behaviors.form_styler = function (context) {
   $('form.form-styler', context).each(function () {
-    $(this).jqTransform({imgPath: Drupal.settings.imgPath});
+    $(this).jqTransform(Drupal.settings.form_styler);
   });
 };
